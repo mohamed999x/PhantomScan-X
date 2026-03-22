ad9de0b676980646b6019dafe50815f741a948a3
Mohamed Abdelhameed <mohamed999x@users.noreply.github.com>
2026-03-22 03:23:07 +0200

    fix: add maskphish files directly (was registered as empty submodule)

---
+++ maskphish/maskphish.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Bash Script for Hide Phishing URL Created by KP
+
+url_checker() {
+    if [ ! "${1//:*}" = http ]; then
+        if [ ! "${1//:*}" = https ]; then
+            echo -e "\e[31m[!] Invalid URL. Please use http or https.\e[0m"
+            exit 1
+        fi
+    fi
+}
+
+echo -n -e "\e[1;32m"
+cat << "EOF"
+                 ___====-_  _-====___
+           _--^^^#####//      \\#####^^^--_
+        _-^##########// (    ) \\##########^-_
+       -############//  |\^^/|  \\############-
+     _/############//   (@::@)   \\############\_
+    /#############((     \\//     ))#############\
+   -###############\\    (oo)    //###############-
+  -#################\\  / VV \  //#################-
+ -###################\\/      \//###################-
+_#/|##########/\######(   /\   )######/\##########|\#_
+|/ |#/\#/\#/\/  \#/\##\  |  |  /##/\#/  \/\#/\#/\#| \|
+`  |/  V  V  `   V  \#\| |  | |/#/  V   '  V  V  \|  '
+   `   `  `      `   / | |  | | \   '      '  '   '
+                    (  | |  | |  )
+                   __\ | |  | | /__
+                  (vvv(VVV)(VVV)vvv)
+EOF
+echo -n -e "\e[0m"
+echo -e "\e[1;31m---------------- Mohamed Abdelhameed --------------------\e[0m\n"
+echo -e "\e[1;31;42m ### Phishing URL ###\e[0m \n"
+echo -n "Paste Phishing URL here (with http or https): "
+read phish
+url_checker $phish
+sleep 1
+echo "Processing and Modifying Phishing URL"
+echo ""
+short=$(curl -s https://is.gd/create.php\?format\=simple\&url\=${phish})
+shorter=${short#https://}
+echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
+echo 'Domain to mask the Phishing URL (with http or https), ex: https://google.com, http://anything.org :'
+echo -en "\e[32m=>\e[0m "
+read mask
+url_checker $mask
+echo -e '\nType social engineering words: (like free-money, best-pubg-tricks)'
+echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0m"
+echo -en "\e[32m=>\e[0m "
+read words
+if [[ -z "$words" ]]; then
+echo -e "\e[31m[!] No words.\e[0m"
+echo -e "\nGenerating MaskPhish Link...\n"
+final=$mask@$shorter
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
+exit
+fi
+if [[ "$words" =~ " " ]]; then
+echo -e "\e[31m[!] Invalid words. Please avoid space.\e[0m"
+echo -e "\nGenerating MaskPhish Link...\n"
+final=$mask@$shorter
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
+exit
+fi
+# Minor fix: Trim trailing newline from shorter variable to avoid malformed URL
+shorter=$(echo -n "$shorter")
+echo -e "\nGenerating MaskPhish Link...\n"
+final=$mask-$words@$shorter
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
